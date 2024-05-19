0470f541e0ad243022e3fb850bd07ecbd2c22a24
gifolha <43251720+gifolha@users.noreply.github.com>
2024-05-19 11:16:20 -0300

    Update techchallenge_4.py

--- techchallenge_4.py
+++ techchallenge_4.py
@@ -149,7 +149,6 @@ def main():
         st.subheader("Challenge")
         st.markdown(
             """
-            O PROBLEMA
             Você foi contratado para uma consultoria, e seu trabalho envolve analisar os dados de preço do petróleo Brent, que pode ser encontrado no site do ipea. Essa base de dados histórica envolve duas colunas: data e preço (em dólares).
 
             Um grande cliente do segmento pediu para que a consultoria desenvolvesse um dashboard interativo e que gere insights relevantes para tomada de decisão. Além disso, solicitaram que fosse desenvolvido um modelo de Machine Learning para fazer o forecasting do preço do petróleo.
