95e6bcef666b1a2877ced8f447e6a5b1016932b5
gifolha <43251720+gifolha@users.noreply.github.com>
2024-05-19 17:38:31 -0300

    Update techchallenge_4.py

--- techchallenge_4.py
+++ techchallenge_4.py
@@ -409,14 +409,15 @@ def main():
     elif choice == "Previsões e Modelo de Machine Learning":
         st.subheader("Previsões e Modelo de Machine Learning")
 
-    # Plotando a série temporal original
+        # Plotando a série temporal original
         fig, ax = plt.subplots(figsize=(15, 8))
         ax.plot(data['Date'], data['Close'], label='Close Price (Original)', color='black')
 
     # Previsões ARIMA
-        arima_data = arima_forecast(data)
-        if arima_data is not None:
-            ax.plot(arima_data['Date'], arima_data['Forecast_ARIMA'], label='ARIMA Forecast', color='red')
+        if not plt.gca().has_data():
+            arima_data = arima_forecast(data)
+            if arima_data is not None:
+                ax.plot(arima_data['Date'], arima_data['Forecast_ARIMA'], label='ARIMA Forecast', color='red')
 
     # Previsões Prophet
         prophet_data = prophet_forecast(data)
@@ -431,6 +432,7 @@ def main():
         ax.set_title('Comparação das Previsões ARIMA, Prophet e LSTM')
         ax.legend()
         st.pyplot(fig)
+
 
     
 
