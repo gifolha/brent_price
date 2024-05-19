9ad8bdb6f47365c878728caf1147ff3d7361449d
gifolha <43251720+gifolha@users.noreply.github.com>
2024-05-19 19:03:45 -0300

    Update techchallenge_4.py

--- techchallenge_4.py
+++ techchallenge_4.py
@@ -463,37 +463,31 @@ def main():
             st.write(predictions_table_styled)
     
 
-
-elif choice == "Conclusão":
-    st.subheader("Conclusão de qual é o melhor modelo")
     
-    # Texto de conclusão
-    st.markdown("Aqui está a conclusão sobre qual é o melhor modelo para previsão do preço do petróleo Brent.")
-    
-    # Verificando se os dados de ARIMA, LSTM e Prophet estão disponíveis
-    if arima_data is not None and lstm_data is not None and prophet_data is not None:
-        # Criando uma tabela com os resultados dos modelos
-        conclusion_table = pd.DataFrame({
-            "Data": data['Date'],
-            "Close Price (Real)": data['Close'],
-            "Previsão ARIMA": arima_data['Forecast_ARIMA'],
-            "Erro ARIMA": arima_data['Erro_ARIMA'],
-            "Previsão LSTM": lstm_data['Forecast_LSTM'],
-            "Erro LSTM": lstm_data['Erro_LSTM'],
-            "Previsão Prophet": prophet_data['yhat'],
-            "Erro Prophet": prophet_data['Erro_Prophet']
-        })
+    elif choice == "Conclusão":
+        st.subheader("Conclusão de qual é o melhor modelo")
         
-        # Estilizando a tabela
-        st.dataframe(conclusion_table.style.set_precision(2))
-    else:
-        st.error("Não foi possível gerar a conclusão devido a dados ausentes.")
-
-elif choice == "Previsões e Modelo de Machine Learning":
-    st.subheader("Previsões e Modelo de Machine Learning")
-    
-    # Aqui vai o código para essa seção
-    # ...
+        # Texto de conclusão
+        st.markdown("Aqui está a conclusão sobre qual é o melhor modelo para previsão do preço do petróleo Brent.")
+        
+        # Verificando se os dados de ARIMA, LSTM e Prophet estão disponíveis
+        if arima_data is not None and lstm_data is not None and prophet_data is not None:
+            # Criando uma tabela com os resultados dos modelos
+            conclusion_table = pd.DataFrame({
+                "Data": data['Date'],
+                "Close Price (Real)": data['Close'],
+                "Previsão ARIMA": arima_data['Forecast_ARIMA'],
+                "Erro ARIMA": arima_data['Erro_ARIMA'],
+                "Previsão LSTM": lstm_data['Forecast_LSTM'],
+                "Erro LSTM": lstm_data['Erro_LSTM'],
+                "Previsão Prophet": prophet_data['yhat'],
+                "Erro Prophet": prophet_data['Erro_Prophet']
+            })
+            
+            # Estilizando a tabela
+            st.dataframe(conclusion_table.style.set_precision(2))
+        else:
+            st.error("Não foi possível gerar a conclusão devido a dados ausentes.")
 
     
     elif choice == "Navegação":
